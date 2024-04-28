c4bb5cffe4b09d9d810aa4431bf834dce740952b
Irakli Berdzenishvili <129960530+bugha123123@users.noreply.github.com>
2024-04-28 22:45:09 +0400

    initial commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,3 @@
-using Hotel.org.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,27 +5,13 @@ namespace Hotel.org.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+      
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    
     }
 }
