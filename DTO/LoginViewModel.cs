95dc293674eb75256abd4bf14591abd13759cdb0
Irakli Berdzenishvili <129960530+bugha123123@users.noreply.github.com>
2024-04-30 15:40:51 +0400

    Added Auth ViewModels

---
+++ DTO/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations; 
+
+
+namespace Hotel.org.DTO
+{
+    // Public class declaration for LoginViewModel
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        
+        [Compare("Password", ErrorMessage = "Passwords do not match")] // Compares ConfirmPassword to Password
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ DTO/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.org.DTO
+{
+    public class RegisterViewModel
+    {
+
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
