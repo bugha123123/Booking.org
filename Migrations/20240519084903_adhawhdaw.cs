171535d063fd342bff73089e63148e3675c1b4a5
Irakli Berdzenishvili <129960530+bugha123123@users.noreply.github.com>
2024-05-19 12:50:36 +0400

    .

---
+++ Migrations/20240519084903_adhawhdaw.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.org.Migrations
+{
+    /// <inheritdoc />
+    public partial class adhawhdaw : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "5796b4e0-69ce-4eda-9606-a4835f3377d2");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "CardCV", "CardExpirationDate", "CardNumber", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "ProfileImageFileName", "SecurityStamp", "TwoFactorEnabled", "UserName", "UserRole" },
+                values: new object[] { "66286696-117b-4102-b5ba-df23085607d5", 0, "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "", "9cf18818-09f3-489a-b27a-7a8ff6ec3baa", "admin@example.com", true, false, null, "ADMIN@EXAMPLE.COM", "ADMIN@EXAMPLE.COM", "AQAAAAIAAYagAAAAENdyr/fi6xhyzhQ0uDKGREJGKjNS+rhj1/++bUC9FP5l4TQCfkI1m7qmAl5DsML9yQ==", null, false, null, "327572ef-b9e6-429a-bdcb-9931f5e664cb", false, "admin@example.com", "ADMIN" });
+
+            migrationBuilder.UpdateData(
+                table: "Flights",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "To",
+                value: "Luxury Resort");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "66286696-117b-4102-b5ba-df23085607d5");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "CardCV", "CardExpirationDate", "CardNumber", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "ProfileImageFileName", "SecurityStamp", "TwoFactorEnabled", "UserName", "UserRole" },
+                values: new object[] { "5796b4e0-69ce-4eda-9606-a4835f3377d2", 0, "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "", "d7e9749a-708a-4ff3-b37f-8917036502ca", "admin@example.com", true, false, null, "ADMIN@EXAMPLE.COM", "ADMIN@EXAMPLE.COM", "AQAAAAIAAYagAAAAEMxdEcxmSom8/8cB+Lox05y6jJH779QtIHRH56kJSkVmNnbaa70ciLLqH8SV2idLjA==", null, false, null, "e3435916-589a-4406-bef2-ecb0c1ef6acc", false, "admin@example.com", "ADMIN" });
+
+            migrationBuilder.UpdateData(
+                table: "Flights",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "To",
+                value: "");
+        }
+    }
+}
